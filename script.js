52f93f97ed5a5a0ac33ba039e81a4c631c8216c4
Andre Diop <itsjustandre@gmail.com>
2020-12-20 16:06:23 -0500

    added search bar functionality and logging of temp, humidity, and wind speed

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-var currentDay = moment().format("[The weather for ] dddd");  
+var currentDay = moment().format("[on ] dddd");
 $("#currentDay").text(currentDay);
 
-console.log(currentDay);
+var cityArray = "";
+
+$("#searchButton").on("click", function (event) {
+  event.preventDefault();
+  var cityName = $("#userInput").val(); //user enters city name to search
+  $("#searchedCity")
+    .text("The forecast for " + cityName)
+    .val(); //name populates on dashboard screen
+
+  var apiKey = "5fda15094fca4dcb78d5eb62c2222c88";
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=imperial&appid=" +
+    apiKey;
+  // AJAX Call
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response.main.temp); //log temperature
+    console.log(response.main.humidity);
+    console.log(response.wind.speed);
+    console.log(response);
+  });
+});
